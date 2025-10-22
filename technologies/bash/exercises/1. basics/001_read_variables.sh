b6e485cc201c16d516e7e10e5f48ff5bf3bec4ac
Alberto Iglesias <alberto@iglesias.cloud>
2025-10-22 15:56:33 -0400

    feat(technologies/bash): Add first basic example of unit testing with bash

---
+++ technologies/bash/exercises/1. basics/001_read_variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+greet_user () {
+    local user_name="$1"
+    echo "Hello user, can you tell me your name?"
+    echo "Hello, lovelly $user_name"
+}
+
+# Only execute if the script is run directly (not sourced)
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "This script is starting!"
+    read -r -p "Please enter your name:" user_name
+    result=$(greet_user "$user_name")
+    echo "The result is: $result"
+fi
