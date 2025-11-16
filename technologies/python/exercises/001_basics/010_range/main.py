d8c40f4f5096200334fb800efbc5ef73ec735cde
Alberto Iglesias <alberto@iglesias.cloud>
2025-11-16 12:37:56 -0500

    feat(python/exercises): Add range function practice to basic exercises

---
+++ technologies/python/exercises/001_basics/010_range/main.py
@@ -0,0 +1,17 @@
+print("\nRange with only a stop: \n")
+x = range(6)
+
+for n in x:
+  print(n)
+
+print("\nRange with an start: \n")
+y = range(5, 10)
+
+for n in y:
+  print(n)
+
+print("\nRange with an start, stop and sequence: \n")
+z = range(3, 10 ,2)
+
+for n in z:
+  print(n)
